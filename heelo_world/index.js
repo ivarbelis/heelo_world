4357eff5d6ae86d6ca79b29dbb1007ea883fa374
Ivar Beliš <belis@oksystem.cz>
2024-11-05 12:03:05 +0100

    Update index.js
    index.js change

--- heelo_world/index.js
+++ heelo_world/index.js
@@ -4,6 +4,6 @@ const port = 3000;
 const server = http.createServer((req, res) => {
  console.log(req.headers);
  res.statusCode = 200;
- res.end('<html><body><h1>Hello, World!!!!!!!</h1></body></html>');
+ res.end('<html><body><h1>Hello, World!!!!!!!OOOOO</h1></body></html>');
 })
 server.listen(port, hostname);
